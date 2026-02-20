f3c5537c2176685437da4ffeba4cbf6267e0397c
Miguel Brito <miguelbrit@gmail.com>
2026-02-20 12:45:48 -0400

    Acomodar Error con Vercel

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -12,6 +12,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
+
 app.get('/', (req, res) => {
   console.log('Root route accessed');
   res.send('API de Dr Goyo activa y funcionando');
